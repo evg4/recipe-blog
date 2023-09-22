48be86cabb5e7b27284f290d34b9ad89c017492f
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2023-09-22 15:12:08 +0100

    add fake email address

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,8 +7,8 @@ function Contact() {
     <div className={styles.container}>
       <h2>Contact</h2>
       <p>
-        Please send me an email if you want to learn more about my cooking
-        journey or to send me your own recommendations!
+        Please send me an email to fake@email.com if you want to learn more
+        about my cooking journey or to send me your own recommendations!
       </p>
       <BackButton />
     </div>
